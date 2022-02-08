cc2e37d20f6dad4f6e3d662b832c3417f59d922a
traiverz <niceconter100@mail.ru>
2022-02-08 21:25:50 +0600

    1

---
+++ border.py
@@ -0,0 +1,24 @@
+import turtle
+class Border():
+    def __init__(self):
+        border = turtle.Turtle()
+        border.hideturtle()
+        border.penup()
+        border.goto(-311, 311)
+        border.pendown()
+        border.goto(311, 311)
+        border.goto(311, -311)
+        border.goto(-311, -311)
+        border.goto(-311, 311)
+        # вторая полоса кроя для красоты
+        border.goto(-313, 313)
+        border.goto(313, 313)
+        border.goto(313, -313)
+        border.goto(-313, -313)
+        border.goto(-313, 313)
+        # третья полоса кроя для красоты
+        border.goto(-315, 315)
+        border.goto(315, 315)
+        border.goto(315, -315)
+        border.goto(-315, -315)
+        border.goto(-315, 315)
---
+++ smile.py
@@ -0,0 +1,22 @@
+import turtle
+class Trey():
+    def __init__(self):
+        # треугольники посреди поля
+        smile = turtle.Turtle()
+        smile.hideturtle()
+        smile.penup()
+        smile.goto(0, 0)
+        smile.pendown()
+        smile.goto(100, 100)
+        smile.goto(100, 0)
+        smile.goto(100, 100)
+        smile.penup()
+        smile.goto(0, 0)
+        smile.pendown()
+        smile.goto(-100, 100)
+        smile.goto(-100, 0)
+        smile.goto(100, 0)
+        smile.penup()
+        smile.goto(0, 0)
+        smile.pendown()
+        smile.shape('circle')
